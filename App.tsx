7764f3a62b248eb59a29636474a7d267e302f041
Michael White <michaelwhite@Michaels-MacBook-Pro.local>
2022-11-10 07:43:56 -0500

    linter warnings

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
+import React from 'react-native';
 import AppStack from './src/navigation/appStack';
 
 export default function App() {
-  return (
-    <AppStack/>
-  );
+    return (
+        <AppStack/>
+    );
 }
